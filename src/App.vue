f038e61e6d89eb6c6fdc3454bf05f79e20430a9f
yapyeeqiang <yapyeeqiang@gmail.com>
2022-07-05 09:56:26 +0800

    update to script setup syntax

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,45 @@
 <template>
-	<h1>Todo App</h1>
-	<form>
-		<label for="newTodo">Enter your todo:</label>
-		<input
-			v-model="store.todoInput.item"
-			type="text"
-			placeholder="Enter your todo"
-		/>
-		<button @click.prevent="store.addTodo()">Add Todo</button>
+    <h1>Todo App</h1>
+    <form>
+        <label for="newTodo">Enter your todo:</label>
+        <input v-model="store.todoInput.item" type="text" placeholder="Enter your todo" />
+        <button @click.prevent="store.addTodo()">Add Todo</button>
 
-		<select v-model="store.filterTodo" name="filterTodo" id="filterTodo">
-			<option value="All">All</option>
-			<option value="Completed">Completed</option>
-			<option value="Incomplete">Incomplete</option>
-		</select>
-	</form>
+        <select v-model="store.filterTodo" name="filterTodo" id="filterTodo">
+            <option value="All">All</option>
+            <option value="Completed">Completed</option>
+            <option value="Incomplete">Incomplete</option>
+        </select>
+    </form>
 
-	<ul>
-		<li v-for="(todo, index) in store.filteredTodos" :key="index">
-			<span
-				:class="[todo.completed ? 'completed-todo' : '']"
-				class="todo-text"
-				>{{ todo.item }}</span
-			>
-			<button @click="store.completeTodo(index)">
-				{{ todo.completed ? 'Undo' : 'Complete' }}
-			</button>
-		</li>
-	</ul>
+    <ul>
+        <li v-for="(todo, index) in store.filteredTodos" :key="index">
+            <span :class="[todo.completed ? 'completed-todo' : '']" class="todo-text">{{ todo.item }}</span>
+            <button @click="store.completeTodo(index)">
+                {{ todo.completed ? 'Undo' : 'Complete' }}
+            </button>
+        </li>
+    </ul>
 </template>
 
-<script>
-import { useTodo } from './useTodo';
-import { onMounted } from 'vue';
+<script setup>
+import { useTodo } from './useTodo'
+import { onMounted } from 'vue'
 
-export default {
-	name: 'todoApp',
-	setup() {
-		const store = useTodo();
+const store = useTodo()
 
-		onMounted(() => {
-			store.todos = JSON.parse(localStorage.getItem('todos'));
-		});
-
-		return {
-			store,
-		};
-	},
-};
+onMounted(() => {
+    store.todos = JSON.parse(localStorage.getItem('todos'))
+})
 </script>
 
 <style scoped>
 .todo-text {
-	padding-right: 10px;
+    padding-right: 10px;
 }
 
 .completed-todo {
-	color: green;
-	text-decoration: line-through;
+    color: green;
+    text-decoration: line-through;
 }
 </style>
